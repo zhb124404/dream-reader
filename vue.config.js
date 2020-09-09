2644a3f826683ef3671af83bc4fb65c408be9299
zhb124404 <1945698666@qq.com>
2020-09-09 09:16:54 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  // eslint-编译时语法检查
+  lintOnSave: true,
+  devServer: {
+    // 浏览器是否自动打开
+    open: true,
+    // 配置跨域代理
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 }
